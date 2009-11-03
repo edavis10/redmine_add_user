37695baa04cde14c5947404d2a5fbacbac019afa
Eric Davis <edavis@littlestreamsoftware.com>
2009-11-03 11:13:45 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_add_user do
+  name 'Redmine Add User plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
