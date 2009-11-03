ef5039c0a0e4e440e6850e0425bc60c77dc679e2
Eric Davis <edavis@littlestreamsoftware.com>
2009-11-03 11:26:19 -0800

    [#3164] Added project documentation and license

--- init.rb
+++ init.rb
@@ -1,8 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_add_user do
-  name 'Redmine Add User plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'Add User is a plugin that will allow project Members to create new users without having Administrator rights.'
+
+  version '0.1.0'
+  requires_redmine :version_or_higher => '0.8.0'
 end
